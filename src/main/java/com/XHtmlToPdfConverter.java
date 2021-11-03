d96e2e166f863cbf62c5afbbbe6a36eb4cecba6e
mangesh <mangesh.hankare@gmail.com>
2021-11-03 16:22:16 -0400

    refactored the code to use html code (string) instead of creating temp file.

--- src/main/java/com/XHtmlToPdfConverter.java
+++ src/main/java/com/XHtmlToPdfConverter.java
@@ -6,130 +6,93 @@ import org.jsoup.Jsoup;
 import org.jsoup.helper.W3CDom;
 import org.jsoup.nodes.Document;
 
-import java.io.*;
-import java.nio.file.FileSystems;
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class XHtmlToPdfConverter {
 
-    final static String OUTPUT_PDF_FILE = "C:\\backup\\Test-7.pdf";
+    // TODO define all constants inside the another class.
+    // START
+    final static String UTF_8 = "UTF-8";
+    final static float PAGE_WIDTH = 240f;
+    final static float PAGE_HEIGHT = 290f;
+    final static BaseRendererBuilder.PageSizeUnits PAGE_SIZE_UNIT = BaseRendererBuilder.PageSizeUnits.MM;
+    // END
 
-    final static String HTML_CODE = "<html lang=\"en\">\n" +
-            "  <head>\n" +
-            "    <title>HTML File</title>  \n" +
-            "    <link href=\"../css/style.css\" rel=\"stylesheet\" >\n" +
-            "  </head>\n" +
-            "  <body>\n" +
-            "    <h1>HTML to PDF Java Example</h1>\n" +
-            "    <p style=\"color:#F80000; font-size:20px\">Inline CSS</p>\n" +
-            "    <p class=\"fontclass\">styling from font</p>\n" +
-            "    <p class=\"styleclass\">external CSS class</p>\n" +
-            "  </body>\n" +
-            "</html>";
+    final static String HTML_CODE = "<h1>HTML to PDF Java Example</h1>";
 
-    final static String HTML_CODE2 = "<html lang=\"en\">\n" +
-            "  <head>\n" +
-            "    <title>HTML File</title>  \n" +
-            "    <link href=\"../css/style.css\" rel=\"stylesheet\" >\n" +
-            "  </head>\n" +
-            "  <body>\n" +
-            "    <h4>HTML to PDF Java Example</h4>\n" +
-            "  </body>\n" +
-            "</html>";
-
-    final static String HTML_CODE3 = "<!DOCTYPE html>\n" +
-            "<html>\n" +
-            "<head>\n" +
-            "<style>\n" +
-            "table, th, td {\n" +
-            "  border: 1px solid black;\n" +
-            "}\n" +
-            "</style>\n" +
-            "</head>\n" +
-            "<body>\n" +
-            "\n" +
-            "<h1>The th element</h1>\n" +
-            "\n" +
-            "<p>The th element defines a header cell in a table:</p>\n" +
-            "\n" +
-            "<table>\n" +
-            "  <tr>\n" +
-            "    <th>Month</th>\n" +
-            "    <th>Savings</th>\n" +
-            "  </tr>\n" +
-            "  <tr>\n" +
-            "    <td>January</td>\n" +
-            "    <td>$100</td>\n" +
-            "  </tr>\n" +
-            "  <tr>\n" +
-            "    <td>February</td>\n" +
-            "    <td>$80</td>\n" +
-            "  </tr>\n" +
-            "</table>\n" +
-            "\n" +
-            "</body>\n" +
-            "</html>\n";
-
-    private XHtmlToPdfConverter() {
+    public XHtmlToPdfConverter() {
         super();
     }
 
-    private File fileFromString(String htmlCode) {
-        File htmlFile = null;
-        FileWriter fileWriter = null;
-        try {
-            htmlFile = File.createTempFile("htmlToPdf-", ".tmp");
-            fileWriter = new FileWriter(htmlFile);
-            fileWriter.write(htmlCode);
-            fileWriter.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                fileWriter.close();
-            } catch (IOException e) {
-                throw new RuntimeException("can not convert HTML TO file");
-            }
-        }
-        return htmlFile;
+    /**
+     * This call will convert the HTML code to the byte[] -
+     * Step 1 : convert HTML to XHTML.
+     * Step 2 : Convert XHTML To PDF byte array.
+     *
+     * @param htmlCode
+     * @return
+     * @throws IOException
+     */
+    final private byte[] convertHtmlToPdf(final String htmlCode) throws IOException {
+        return xhtmlToPdf(xHtmlFromString(HTML_CODE));
     }
 
+    /**
+     * NOTE - THIS IS THE TEST METHOD
+     * TEST METHOD
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         try {
-            XHtmlToPdfConverter converter = new XHtmlToPdfConverter();
-            File htmlFile = converter.fileFromString(HTML_CODE);
-            //File htmlFile = new File("C:\\Users\\mange\\Downloads\\report_html.html");
-            org.w3c.dom.Document docUsingJsoup = converter.createXHtml(htmlFile);
-            converter.xhtmlToPdf(docUsingJsoup, OUTPUT_PDF_FILE);
+            final XHtmlToPdfConverter converter = new XHtmlToPdfConverter();
+            byte[] pdfBytes = converter.convertHtmlToPdf(HTML_CODE);
+
+            //Below 3 lines are to test the convertHtmlToPdf method.
+            OutputStream out = new FileOutputStream("C:\\backup\\Test-14.pdf");
+            out.write(pdfBytes);
+            out.close();
         } catch (IOException e) {
-            System.out.println("Error, converting HTML to PDF " + e.getMessage());
-            e.printStackTrace();
+            System.out.println("Error, converting HTML to PDF " + e.getMessage()); // TODO Logger
         }
     }
 
-    private org.w3c.dom.Document createXHtml(File inputHTML) throws IOException {
-        Document document = Jsoup.parse(inputHTML, "UTF-8");
+    /**
+     * @param htmlCode
+     * @return
+     */
+    final private org.w3c.dom.Document xHtmlFromString(final String htmlCode) {
+        final Document document = Jsoup.parse(htmlCode, UTF_8);
         document.outputSettings().syntax(Document.OutputSettings.Syntax.xml);
-        System.out.println("created XHtml document.");
         return new W3CDom().fromJsoup(document);
     }
 
-
-    private void xhtmlToPdf(org.w3c.dom.Document doc, String outputPdf) throws IOException {
-        String baseUri = FileSystems.getDefault()
-                .getPath("C:/", "backup/", "src/main/resources")
-                .toUri()
-                .toString();
-        OutputStream os = new FileOutputStream(outputPdf);
-        PdfRendererBuilder builder = new PdfRendererBuilder();
-        builder.useDefaultPageSize(260, 300, BaseRendererBuilder.PageSizeUnits.MM);
-
-        builder.withUri(outputPdf);
-        builder.toStream(os);
-        // we can add extra fonts, if needed.
-        //builder.useFont(new File(getClass().getClassLoader().getResource("fonts/PRISTINA.ttf").getFile()), "PRISTINA");
-        builder.withW3cDocument(doc, baseUri);
-        builder.run();
-        System.out.println("COMPLETED..");
-        os.close();
+    /**
+     * If anything goes wrong, no point to send the letter so we need to stop the current letter generation with error.
+     * Do you agree?
+     *
+     * @param doc
+     * @return
+     * @throws IOException
+     */
+    final private byte[] xhtmlToPdf(final org.w3c.dom.Document doc) throws IOException {
+        final ByteArrayOutputStream pdfStream = new ByteArrayOutputStream();
+        try {
+            final PdfRendererBuilder builder = new PdfRendererBuilder();
+            builder.useDefaultPageSize(PAGE_WIDTH, PAGE_HEIGHT, PAGE_SIZE_UNIT)
+                    .toStream(pdfStream)
+                    .withW3cDocument(doc, null)
+                    .run();
+            return pdfStream.toByteArray();
+        } catch (IOException e) {
+            // TODO add logger with the error and create Exception classes, depending on business cases.
+            throw new RuntimeException("we can not proceed the request because of ", e);
+        } finally {
+            pdfStream.close();
+            // TODO Logger to indicate SUCCESS, with some ID or Reference.
+        }
     }
 }
